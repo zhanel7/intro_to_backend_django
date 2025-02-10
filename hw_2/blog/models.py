1d44572467a2fc2b2214fee926aa95f4d0855b52
zhanel7 <zhanelyerik@gmail.com>
2025-02-11 00:53:21 +0300

    Добавлены шаблоны и оформление

---
+++ hw_2/blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    author = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
