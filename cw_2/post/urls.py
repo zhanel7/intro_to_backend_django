92f54b0ab668264d064652d5015fe789a14cf98a
zhanel7 <zhanelyerik@gmail.com>
2025-02-11 01:17:47 +0300

    Удалил старый проект cw_2

--- cw_2/post/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import post_list, post_detail, post_create, post_delete
-
-urlpatterns = [
-    path('', post_list, name='post-list'),  # GET posts/
-    path('<int:id>/', post_detail, name='post-detail'),  # GET posts/:id
-    path('create/', post_create, name='post-create'),  # POST posts/
-    path('<int:id>/delete/', post_delete, name='post-delete'),  # DELETE posts/:id/delete
-]
