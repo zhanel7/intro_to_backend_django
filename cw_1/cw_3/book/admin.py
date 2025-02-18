4d626ab9a240f6bb7bcba634cb55ff9935b589b4
zhanel7 <zhanelyerik@gmail.com>
2025-02-18 12:39:54 +0300

    Удален cw_1

--- cw_1/cw_3/book/admin.py
+++
@@ -1,8 +0,0 @@
-# book/admin.py
-from django.contrib import admin
-from .models import Book
-
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # Отображаем поля в списке
-    search_fields = ('title', 'author')  # Добавляем возможность поиска по этим полям
