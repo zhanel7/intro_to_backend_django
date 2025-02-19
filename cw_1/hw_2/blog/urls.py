89f6307547bdda6880df5a237fe4dd6b6ca83ad1
zhanel7 <zhanelyerik@gmail.com>
2025-02-19 07:35:04 +0300

    cw_1 rep fix

--- cw_1/hw_2/blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import article_list, article_detail
-
-urlpatterns = [
-    path('articles/', article_list, name='article_list'),
-    path('articles/<int:article_id>/', article_detail, name='article_detail'),
-]
--- cw_1/todo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.todo_list, name='todo_list'),
-    path('<int:id>/', views.todo_detail, name='todo_detail'),
-    path('create/', views.create_todo, name='create_todo'),
-    path('<int:id>/delete/', views.todo_delete, name='todo_delete'),
-]
